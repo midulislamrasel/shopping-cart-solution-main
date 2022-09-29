c865d0622124e510c090b69a05c1dff803dc1f23
Midul-islam-Russell <midulraddy121@gmail.com>
2022-09-29 23:15:01 +0600

    phoneCase

--- js/phone.js
+++ js/phone.js
@@ -3,8 +3,8 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
     const phoneNumberFiled = document.getElementById('phone-number-field');
     const phoneNumberValue = phoneNumberFiled.value;
     const phoneNumberString = parseInt(phoneNumberValue);
-    phoneNumberFiled.value = phoneNumberString + 1;
-    console.log(phoneNumberValue)
+    const phoneNumber = phoneNumberFiled.value = phoneNumberString + 1;
+    
 });
 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
@@ -12,22 +12,23 @@ document.getElementById('btn-phone-minus').addEventListener('click', function ()
     const phoneNumberValue = phoneNumberFiled.value;
     const phoneNumberString = parseInt(phoneNumberValue);
     phoneNumberFiled.value = phoneNumberString - 1;
-    // console.log(phoneNumberValue)
+    
 });
 
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     const phoneCaseInput = document.getElementById('case-number-field');
     const phoneCaseNumber = phoneCaseInput.value;
     const phoneCaseString = parseInt(phoneCaseNumber);
-    phoneCaseInput.value = phoneCaseString + 1;
-    console.log(phoneCaseNumber)
+    const phoneCase =  phoneCaseInput.value = phoneCaseString + 1;
+    
 
-    const phonePriceInput = document.getElementById('phone-total');
+    const phonePriceInput = document.getElementById('case-total');
     const phonePriceValue = phonePriceInput.innerText
     const phonePriceSting = parseInt(phonePriceValue);
-    phonePriceInput.innerText = phonePriceSting * phoneCaseNumber;
+    const phonePrice = phonePriceInput.innerText = phonePriceSting * phoneCase;
+    console.log(phonePriceSting ,phonePrice)
 
-    console.log(phonePriceSting)
+    
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
